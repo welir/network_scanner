839091fb59fe267a71593c0df7e3a83a654c98e4
Denis Voronin <fhoc@narod.ru>
2025-02-04 10:25:00 +0300

    Update test_scanner.sh

--- tests/test_scanner.sh
+++ tests/test_scanner.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-test_scan_localhost() {
-    result=$(./src/network_scanner.sh -p 80,443 -o text)
-    assertContains "$result" "80" 
-    assertContains "$result" "443"
+testLocalhostScan() {
+    result=$(./src/network_scanner.sh -p 80 -o text)
+    assertContains "$result" "80/tcp"
 }
-
 # Загрузка shunit2
 source /usr/share/shunit2/shunit2
