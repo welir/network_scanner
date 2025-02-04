dcf2f0c24238efc23207dc32d03dd25499e556e6
Denis Voronin <fhoc@narod.ru>
2025-02-04 09:08:34 +0300

    Create test_scanner.sh

---
+++ tests/test_scanner.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+test_scan_localhost() {
+    result=$(./src/network_scanner.sh -p 80,443 -o text)
+    assertContains "$result" "80" 
+    assertContains "$result" "443"
+}
+
+# Загрузка shunit2
+source shunit2
